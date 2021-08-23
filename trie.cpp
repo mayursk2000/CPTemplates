03b733998700d1b0ce22cb3d58b59d7a5c48a7f2
akgit4713 <46869438+akgit4713@users.noreply.github.com>
2021-08-23 22:07:36 +0530

    Update trie.cpp

--- trie.cpp
+++ trie.cpp
@@ -1,80 +1,70 @@
-/** Initialize your data structure here. */
-    
-    struct Node{
-        int cnt=0;
-        Node* nxt[26];
-        
-        Node(){
-            for(int i=0;i<26;i++)
-                nxt[i]=NULL;
-        }
-    };
-    
-    Node* head;
-    
-    Trie() {
-        head= new Node();
-    }
-    
-    /** Inserts a word into the trie. */
-    void insert(string word) {
-        Node* cur = head;
-        int n=word.length();
-		for(int i = 0; i< n; i++)
-		{
-			int b =word[i]-'a';
-			if(!cur -> nxt[b])
-				cur -> nxt[b] = new Node();
-			cur = cur -> nxt[b];
-			cur -> cnt++;
-		}
-    }
-    
-    /** Returns if the word is in the trie. */
-    bool search(string word) {
-        Node* cur = head;
-        int n=word.length();
-        int cou=0;
-		for(int i = 0; i< n; i++)
-		{
-			int b =word[i]-'a';
-			if(!cur -> nxt[b]){
-                return false;
+struct Trie{
+        struct Node{
+            int cnt=0;
+            Node* nxt[26];
+                
+            Node(){
+                for(int i=0;i<26;i++)
+                    nxt[i]=NULL;
             }
-			cur = cur -> nxt[b];
+        };
+        Node* head;
             
-		}
-        int c=cur->cnt;
-        for(int i=0;i<26;i++){
-            if(cur->nxt[i]!=NULL)
-                cou+=cur->nxt[i]->cnt;
+        Trie() {
+            head= new Node();
         }
-        if(cou<c)
-        return true;
-        else
-            return false;
-    }
-    
-    /** Returns if there is any word in the trie that starts with the given prefix. */
-    bool startsWith(string prefix) {
-        Node* cur = head;
-        int n=prefix.length();
-		for(int i = 0; i< n; i++)
-		{
-			int b =prefix[i]-'a';
-			if(!cur -> nxt[b]){
-                return false;
+            
+        /** Inserts a word into the trie. */
+        void insert(string word) {
+            Node* cur = head;
+            int n=word.length();
+            for(int i = 0; i< n; i++)
+            {
+                int b =word[i]-'a';
+                if(!cur -> nxt[b])
+                    cur -> nxt[b] = new Node();
+                cur = cur -> nxt[b];
+                cur -> cnt++;
             }
-			cur = cur -> nxt[b];
-		}
-        return true;
-    }
+        }
+            
+        /** Returns if the word is in the trie. */
+        bool search(string word) {
+            Node* cur = head;
+            int n=word.length();
+            int cou=0;
+            for(int i = 0; i< n; i++)
+            {
+                int b =word[i]-'a';
+                if(!cur -> nxt[b]){
+                    return false;
+                }
+                cur = cur -> nxt[b];
+                    
+            }
+            int c=cur->cnt;
+            for(int i=0;i<26;i++){
+                if(cur->nxt[i]!=NULL)
+                    cou+=cur->nxt[i]->cnt;
+            }
+            if(cou<c)
+                return true;
+            else
+                return false;
+        }
+            
+        /** Returns if there is any word in the trie that starts with the given prefix. */
+        bool startsWith(string prefix) {
+            Node* cur = head;
+            int n=prefix.length();
+            for(int i = 0; i< n; i++)
+            {
+                int b =prefix[i]-'a';
+                if(!cur -> nxt[b]){
+                    return false;
+                }
+                cur = cur -> nxt[b];
+            }
+            return true;
+        }
 };
-
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie* obj = new Trie();
- * obj->insert(word);
- * bool param_2 = obj->search(word);
- * bool param_3 = obj->startsWith(prefix);
- */
